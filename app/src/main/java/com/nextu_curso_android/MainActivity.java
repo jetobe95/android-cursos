0eb32f6392cac1746bd62280a97482c23687e77c
Jeffrey Torres <jtorres@technisupport.com>
2019-11-28 15:14:10 -0500

    Quede la seccion 6

--- app/src/main/java/com/nextu_curso_android/MainActivity.java
+++ app/src/main/java/com/nextu_curso_android/MainActivity.java
@@ -2,16 +2,27 @@ package com.nextu_curso_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import  com.z.librerias.Promedio;
+import com.googleapis.maps.*;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
         TextView textView = findViewById(R.id.textView);
         textView.setText(R.string.baloncesto);
+        final Promedio prome = new Promedio();
+        Log.i("TAG",prome.calavg());
 
 
     }
---
+++ librerias/src/main/java/com/z/librerias/Promedio.java
@@ -0,0 +1,9 @@
+package com.z.librerias;
+
+public class Promedio {
+
+
+    public String calavg(){
+        return "Hola desde la libreria";
+    }
+}

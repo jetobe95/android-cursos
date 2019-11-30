7746da64d808807517fa6bf1e5f95b7feb96a904
Jeffrey Torres <jtorres@technisupport.com>
2019-11-29 19:20:58 -0500

    Guardando estado de las activities

--- app/src/main/java/com/nextu_curso_android/MainActivity.java
+++ app/src/main/java/com/nextu_curso_android/MainActivity.java
@@ -1,36 +1,79 @@
 package com.nextu_curso_android;
 
 import android.content.Intent;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText numA;
-    private EditText numB;
+    private  static  final String STATE_MESSAGE = "Estado mensaje";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        numA = findViewById(R.id.numberA);
-        numB = findViewById(R.id.numberB);
+        Log.wtf("Ciclo de vida","onCreate");
+
 
     }
 
-    public  void navegar(View view){
-        double txtA = Double.parseDouble(numA.getText().toString());
-        double txtB = Double.parseDouble(numB.getText().toString());
-        Intent intent = new Intent(this,Navegacion.class);
-        intent.putExtra("a",txtA);
-        intent.putExtra("b",txtB);
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.wtf("Ciclo de vida","onStart");
+    }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.wtf("Ciclo de vida","onResume");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.wtf("Ciclo de vida","onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.wtf("Ciclo de vida","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.wtf("Ciclo de vida","onDestroy");
+    }
+
+    public void navegar(View view ){
+        Intent intent =  new Intent(this,Navegacion.class);
 
         startActivity(intent);
     }
 
 
+    public void mostrarTexto(View view ){
+
+    }
+
+
+
+    @Override
+    public void onSaveInstanceState( Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        //outState.putString(STATE_MESSAGE);
+
+    }
 }
--- app/src/main/java/com/nextu_curso_android/Navegacion.java
+++ app/src/main/java/com/nextu_curso_android/Navegacion.java
@@ -1,7 +1,10 @@
 package com.nextu_curso_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 public class Navegacion extends AppCompatActivity {
@@ -15,9 +18,40 @@ public class Navegacion extends AppCompatActivity {
         mensaje2 = findViewById(R.id.mensaje2);
         double a = (double) getIntent().getDoubleExtra("a",0.0);
         double b = (double) getIntent().getDoubleExtra("b",0.0);
-
         mensaje2.setText("EL resultado es  "+ (a+b) );
-
-
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.wtf("Ciclo de vida 2","onStart");
+    }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.wtf("Ciclo de vida 2","onResume");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.wtf("Ciclo de vida 2","onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.wtf("Ciclo de vida 2","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.wtf("Ciclo de vida 2","onDestroy");
+    }
+
 }

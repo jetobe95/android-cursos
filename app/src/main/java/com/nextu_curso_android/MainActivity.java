8e832ce86e7989e96e474c2442890b83d4230e38
Jeffrey Torres <jtorres@technisupport.com>
2019-11-30 21:10:38 -0500

    Quede en la seccion de ArrayAdapater de Spinners

--- app/src/main/java/com/nextu_curso_android/MainActivity.java
+++ app/src/main/java/com/nextu_curso_android/MainActivity.java
@@ -4,7 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
+import com.nextu_curso_android.adapter.DiaHorarioAdapter;
+import com.nextu_curso_android.modelo.DiaHorario;
+
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -12,13 +18,24 @@ public class MainActivity extends AppCompatActivity {
 
     private  static  final String STATE_MESSAGE = "Estado mensaje";
 
-
+    private Spinner spinner;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ejercicio_adapter_1);
+        setContentView(R.layout.activity_spinners);
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.horario_de_clases,android.R.layout.simple_spinner_item);
+        List<DiaHorario> horarios = new ArrayList<>();
+
+        horarios.add(new DiaHorario("Mater","Dia"));
+        DiaHorarioAdapter diaHorarioAdapter = new DiaHorarioAdapter(this,horarios);
+        spinner = findViewById(R.id.sp1);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
 
 
 
@@ -26,14 +43,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void irAlarmas(View view){
-        Intent intent = new Intent(this,ListaAlarmas.class);
-        startActivity(intent);
-    }
 
-    public void irReproctor(View view){
-        Intent intent = new Intent(this,ReproductorActivity.class);
-        startActivity(intent);
-    }
 
 }
